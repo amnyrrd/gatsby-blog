fcb9e7171495e222c46621c9afd55aa8ee8598bc
amnyrrd <amnyrrd@gmail.com>
2020-10-01 16:42:57 -0700

    simple blog using gatsby and graphql

---
+++ src/pages/page-3.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>Hellloooooo from #3</h1>
+  </Layout>
+)
+
+export default ThirdPage
